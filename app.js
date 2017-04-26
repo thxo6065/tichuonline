4e64bd3bc01ff3b0f0ec8fa9d1a11a3ce939251a
gihwan <0x1306e6d@gmail.com>
2017-04-26 21:38:26 +0900

    cover 페이지 추가 및 페이지 디자인

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var cover = require('./routes/cover');
+
 var app = express();
 
 // view engine setup
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/cover', cover);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
