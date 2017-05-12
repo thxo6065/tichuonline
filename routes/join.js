5bae83e70ed51ffc88ec9073a9c72906562caaf5
Gihwan Kim <0x1306e6d@gmail.com>
2017-05-13 00:03:51 +0900

    회원가입시 이미 존재하는 계정 검사
    계정 검사와 회원 가입을 함수로 추출
    존재하지 않는 이메일에 한해서 회원가입 할 수 있도록 수정

--- routes/join.js
+++ routes/join.js
@@ -7,6 +7,38 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    findAccount(email, function (err, exists) {
+        if (err) {
+            console.log("Failed to findAccount.");
+            console.error(err);
+
+            res.status(500).redirect('/join');
+            return;
+        }
+
+        if (exists) {
+            res.status(200).redirect('/join');
+        } else {
+            insertAccount(email, password, function (err) {
+                if (err) {
+                    console.log("Failed to insert new account.");
+                    console.error(err);
+
+                    res.status(500).redirect('/join');
+                } else {
+                    console.log("New account was inserted. email=" + email);
+
+                    res.status(200).redirect('/');
+                }
+            });
+        }
+    });
+});
+
+function findAccount(email, callback) {
     mariadb.get(function (err, con) {
         if (err) {
             console.log("Failed to get mariadb.");
@@ -15,28 +47,48 @@ router.post('/', function (req, res) {
             return;
         }
 
-        var email = req.body.email;
-        var password = req.body.password;
+        var sql = "SELECT email FROM account WHERE email=?";
+        var data = [email];
+
+        var exec = con.query(sql, data, function (err, result) {
+            con.release();
+            console.log("[checkAccountExists] SQL : " + exec.sql);
+
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(err, (result.length > 0));
+            }
+        });
+    });
+};
+
+function insertAccount(email, password, callback) {
+    mariadb.get(function (err, con) {
+        if (err) {
+            console.log("Failed to get mariadb.");
+            console.error(err);
+            con.release();
+            return;
+        }
 
         var sql = "INSERT INTO account SET ?";
         var data = {
             email: email,
-            password: password;
+            password: password
         };
 
         var exec = con.query(sql, data, function (err, result) {
             con.release();
-            console.log("SQL : " + exec.sql);
+            console.log("[insertAccount] SQL : " + exec.sql);
 
             if (err) {
-                console.log("Failed to insert new account.");
-                console.error(err);
-                res.status(500).redirect('/join');
+                callback(err);
             } else {
-                res.status(200).redirect('/');
+                callback(null);
             }
         });
     });
-});
+}
 
 module.exports = router;
