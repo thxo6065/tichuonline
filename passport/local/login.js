5414d585fc276ede02f0bacbeac9dcbc4a82b4b7
Gihwan Kim <0x1306e6d@gmail.com>
2017-05-13 18:09:14 +0900

    로그인 기능 구현
    mariadb 데이터베이스의 계정 정보로 로그인
    로그인 실패 시 실패 메시지 출력

--- passport/local/login.js
+++ passport/local/login.js
@@ -1,11 +1,52 @@
 var LocalStrategy = require('passport-local').Strategy;
+var mariadb = require('../../database/mariadb');
+
+function doLogin(email, password, callback) {
+    mariadb.get(function (err, con) {
+        if (err) {
+            console.log("Failed to get mariadb.");
+            console.error(err);
+            con.release();
+            return;
+        }
+
+        var sql = "SELECT id, email FROM account WHERE email=? AND password=?";
+        var data = [email, password];
+
+        var exec = con.query(sql, data, function (err, result) {
+            con.release();
+            console.log("[doLogin] SQL : " + exec.sql);
+
+            if (err) {
+                callback(err, null);
+            } else {
+                if (result.length > 0) {
+                    callback(null, {
+                        id: result.id,
+                        email: result.email
+                    });
+                } else {
+                    callback(null, null);
+                }
+            }
+        });
+    });
+};
 
 module.exports = new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
 }, function (req, email, password, done) {
-    console.log("Called login-local passport. email=" + email);
+    doLogin(email, password, function (err, user) {
+        if (err) {
+            return done(err);
+        }
 
-    done(true);
+        if (!user) {
+            return done(null, false, req.flash('loginMessage', "올바른 계정이 없습니다."));
+        }
+
+        return done(null, user);
+    });
 });
