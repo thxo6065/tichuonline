d536b89db2e49c4cf3cf158a384443180cc2f6a3
Gihwan Kim <0x1306e6d@gmail.com>
2017-05-15 00:08:34 +0900

    WebStorm 프로젝트로 포팅
    WebStorm NodeJS 프로젝트로 포팅
    Bootstrap 파일들 bootstrap 폴더로 이동 후 프로젝트에서 Exclude

--- app.js
+++ app.js
@@ -28,14 +28,14 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: 'secrert-key',
-  resave: false,
-  saveUninitialized: true
+    secret: 'secrert-key',
+    resave: false,
+    saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -52,20 +52,20 @@ app.use('/create', create);
 // app.use('/create', create);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 var initDatabase = require('./init/database');
--- routes/create.js
+++ routes/create.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function (req, res) {
-  // res.send('Hi, post');
-  // var room_name = req.param('room_name');
-  console.log(req.param('room_name'), req.email);
-  res.redirect('/play');
+    // res.send('Hi, post');
+    // var room_name = req.param('room_name');
+    console.log(req.param('room_name'), req.email);
+    res.redirect('/play');
 });
 
 module.exports = router;
